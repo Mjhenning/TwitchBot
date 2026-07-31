a537f479452b7ff4383c149fe1a6cb941d8b10a1
mjhenning <mjhenning2003@gmail.com>
2026-07-31 18:53:48 +0200

    slim tweaking delay 1

--- modules/media_requests/obsController.js
+++ modules/media_requests/obsController.js
@@ -33,7 +33,7 @@ async function enableMediaGroup() {
 
 async function disableMediaGroup() {
     await ensureConnected();
-    await delay(1300);
+    await delay(1150);
     await obs.call('SetSceneItemEnabled', {sceneName: config.OBS_SCENE_NAME, sceneItemId, sceneItemEnabled: false});
 }
 
