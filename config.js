b8ef1e673281270d29e1ef9b2144005177f14a7f
mjhenning <mjhenning2003@gmail.com>
2026-07-31 17:57:49 +0200

    Small OBS flow change for media requests

--- config.js
+++ config.js
@@ -63,7 +63,8 @@ const config = {
 
     OBS_WS_URL: 'ws://192.168.1.71:4455',
     OBS_SCENE_NAME: 'Main',
-    OBS_SOURCE_NAME: 'Media_Playback',
+    OBS_GROUP_NAME: 'Media_Playback',
+    OBS_SOURCE_NAME: 'MediaRequests',
 
     // ---------- Data ----------
     COUNTER_PATH,
